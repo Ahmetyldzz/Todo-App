1867ebf19ae2077eacd3733ec7ae4e902f110aca
Ahmet Yildiz <ahmet91642@gmail.com>
2024-03-30 03:34:27 +0300

    degistirildi

--- lib/todo_app/model/todo.dart
+++ lib/todo_app/model/todo.dart
@@ -18,3 +18,44 @@ class ToDo {
     ];
   }
 }
+
+class TodoModel {
+  int? id;
+  String? toDoText;
+  int? isDone;
+
+  TodoModel({this.id, this.toDoText, this.isDone = 0});
+
+  TodoModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    toDoText = json['toDoText'];
+    isDone = json['isDone'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['toDoText'] = this.toDoText;
+    data['isDone'] = this.isDone;
+    return data;
+  }
+}
+
+class TodoModel1 {
+  int? id;
+  String? toDoText;
+
+  TodoModel1({this.id, this.toDoText});
+
+  TodoModel1.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    toDoText = json['toDoText'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['toDoText'] = this.toDoText;
+    return data;
+  }
+}
